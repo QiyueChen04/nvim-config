eec959c3cbcd1c016d4339ffe7c1cf50c486d353
Qiyue Chen <jackchen20048@gmail.com>
2025-01-20 19:27:45 -0500

    neoscroll

--- lua/plugins/cinnamon.lua
+++ lua/plugins/cinnamon.lua
@@ -11,7 +11,7 @@ return {
     -- Custom scroll options
     options = {
       mode = "cursor",  -- Animate cursor and window scrolling for any movement
-      delay = 5,        -- Delay between each movement step (in ms)
+      delay = 3,        -- Delay between each movement step (in ms)
       step_size = {
         vertical = 1,   -- Number of cursor/window lines moved per step
         horizontal = 1, -- Number of cursor/window columns moved per step
@@ -19,7 +19,7 @@ return {
       max_delta = {
         line = false,   -- Maximum distance for line movements before scroll animation is skipped
         column = false, -- Maximum distance for column movements before scroll animation is skipped
-        time = 1000,    -- Maximum duration for a movement (in ms)
+        time = 10000,    -- Maximum duration for a movement (in ms)
       },
       -- Optional post-movement callback
       callback = function()
---
+++ lua/plugins/neoscroll.lua
@@ -0,0 +1,49 @@
+return {
+  'karb94/neoscroll.nvim',
+  opts = {},
+  config = function()
+    require('neoscroll').setup({
+      hide_cursor = true, -- Hide cursor while scrolling
+      stop_eof = true, -- Stop at <EOF> when scrolling downwards
+      respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+      cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+      duration_multiplier = 1.0, -- Global duration multiplier
+      easing = 'linear', -- Default easing function
+      pre_hook = nil, -- Function to run before the scrolling animation starts
+      post_hook = nil, -- Function to run after the scrolling animation ends
+      performance_mode = false, -- Disable "Performance Mode" on all buffers.
+      ignored_events = { -- Events ignored while scrolling
+        'WinScrolled',
+        'CursorMoved',
+      },
+    })
+    -- Define custom key mappings for Neoscroll
+    local keymap = {
+      ['<C-u>'] = function() require('neoscroll').ctrl_u(1, { duration = 250 }) end,
+      ['<C-d>'] = function() require('neoscroll').ctrl_d(1, { duration = 250 }) end,
+      ['<C-h>'] = function() require('neoscroll').ctrl_b({ duration = 450 }) end,
+      ['<C-l>'] = function() require('neoscroll').ctrl_f({ duration = 450 }) end,
+      ['<C-k>'] = function()
+        require('neoscroll').scroll(
+          -1,
+          { move_cursor = false, duration = 1}
+        )
+      end,
+      ['<C-j>'] = function()
+        require('neoscroll').scroll(
+          1,
+          { move_cursor = false, duration = 1 }
+        )
+      end,
+      ['zt'] = function() require('neoscroll').zt({ half_win_duration = 250 }) end,
+      ['zz'] = function() require('neoscroll').zz({ half_win_duration = 250 }) end,
+      ['zb'] = function() require('neoscroll').zb({ half_win_duration = 250 }) end,
+    }
+
+    -- Set key mappings for normal, visual, and select modes
+    local modes = { 'n', 'v', 'x' }
+    for key, func in pairs(keymap) do
+      vim.keymap.set(modes, key, func)
+    end
+  end,
+}
